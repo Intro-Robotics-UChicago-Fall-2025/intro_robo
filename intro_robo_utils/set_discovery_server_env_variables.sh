916471e16df4d22d811804732a484842b34bde43
Sarah Sebo <sarah.m.sebo@gmail.com>
2025-09-22 11:26:05 -0500

    Updating bash script(s) to enable saving of robot and discovery server settings

--- intro_robo_utils/set_discovery_server_env_variables.sh
+++ intro_robo_utils/set_discovery_server_env_variables.sh
@@ -1,6 +1,6 @@
 # Function to set ROS environment variables based on domain ID
 set_robot_num() {
-  # Mapping from ROS_DOMAIN_ID to ROBOT_IP_ADDRESS
+  # Mapping from ROS_DOMAIN_ID to ROBOT_IP
   declare -A ROBOT_IPS=(
     [1]="128.135.203.20"
     [2]="128.135.203.21"
@@ -26,8 +26,10 @@ set_robot_num() {
   fi
 
   if [[ -n "${ROBOT_IPS[$domain_id]}" ]]; then
+    [ -t 0 ] && export ROS_SUPER_CLIENT=True || export ROS_SUPER_CLIENT=False
+    export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
     export ROS_DOMAIN_ID=$domain_id
-    export ROBOT_IP_ADDRESS="${ROBOT_IPS[$domain_id]}"
+    export ROBOT_IP="${ROBOT_IPS[$domain_id]}"
 
     # Find the maximum index in the mapping
     local max_id
@@ -46,9 +48,22 @@ set_robot_num() {
 
     export ROS_DISCOVERY_SERVER="$list"
 
-    echo "Set ROS_DOMAIN_ID=$ROS_DOMAIN_ID"
-    echo "Set ROBOT_IP_ADDRESS=\"$ROBOT_IP_ADDRESS\""
-    echo "Set ROS_DISCOVERY_SERVER=\"$ROS_DISCOVERY_SERVER\""
+    # Name of the script to generate
+    OUTPUT_SCRIPT="$HOME/intro_robo_ws/src/intro_robo/intro_robo_utils/discovery_server_env_variables.sh"
+
+    # Write commands into the file
+    eval "cat > \"$OUTPUT_SCRIPT\" << EOF
+[ -t 0 ] && export ROS_SUPER_CLIENT=True || export ROS_SUPER_CLIENT=False
+export RMW_IMPLEMENTATION=rmw_fastrtps_cpp
+export ROS_DOMAIN_ID=$domain_id
+export ROBOT_IP=\"${ROBOT_IPS[$domain_id]}\"
+export ROS_DISCOVERY_SERVER=\"$list\"
+EOF"
+
+    echo "Created $OUTPUT_SCRIPT with:"
+    echo -e "\tROS_DOMAIN_ID=$ROS_DOMAIN_ID"
+    echo -e "\tROBOT_IP=\"$ROBOT_IP\""
+    echo -e "\tROS_DISCOVERY_SERVER=\"$ROS_DISCOVERY_SERVER\""
   else
     echo "Error: ROS_DOMAIN_ID $domain_id not found in mapping."
     echo "Available IDs: ${!ROBOT_IPS[@]}"
